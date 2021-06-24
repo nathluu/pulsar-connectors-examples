7418c331d37772f0c17a574c8ed1d7bf14ac8ead
nathluu <luuhoangtanbis@gmail.com>
2021-06-24 19:07:58 +0700

    simple spark pulsar application

---
+++ spark/pulsar-spark-wordcount/build.sbt
@@ -0,0 +1,37 @@
+resolvers ++= Seq (
+  Resolver.mavenLocal,
+  DefaultMavenRepository,
+  "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+  "Stream native Bintray" at "https://dl.bintray.com/streamnative/maven"
+)
+
+lazy val root = (project in file(".")).
+  settings(
+    name := "pulsarspark",
+    version := "0.0.1-SNAPSHOT",
+    scalaVersion := "2.11.12",
+//    assembly / test := {},
+    mainClass in Compile := Some("com.humana.dhp.spark.jobs.WordCount")
+//    mainClass in assembly := Some("com.humana.dhp.spark.jobs.WordCount")
+  )
+
+val sparkVersion = "2.4.5"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
+  "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
+  "io.streamnative.connectors" %% "pulsar-spark-connector" % "2.4.6-SNAPSHOT" % "provided"
+//  "io.streamnative.connectors" %% "pulsar-spark-connector" % "2.4.5"
+)
+
+//assemblyMergeStrategy in assembly := {
+//  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+//  case x => MergeStrategy.first
+//}
+
+//assembly / assemblyMergeStrategy := (x => MergeStrategy.first)
+//
+//assemblyShadeRules in assembly := Seq(
+//  ShadeRule.rename("io.netty.**" -> "shadeio.@1").inAll,
+//  ShadeRule.rename("com.fasterxml.jackson.**" -> "shadeio.@1").inAll
+//)
---
+++ spark/pulsar-spark-wordcount/src/main/scala/com/humana/dhp/spark/jobs/WordCount.scala
@@ -0,0 +1,46 @@
+package com.humana.dhp.spark.jobs
+
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object WordCount extends App {
+  val spark = SparkSession.builder()
+    .appName("WordCount")
+    .master("local[2]")
+    .getOrCreate()
+
+  import spark.implicits._
+  spark.sparkContext.setLogLevel("ERROR")
+
+
+  val lines = spark.readStream
+    .format("pulsar")
+    .option("service.url", "pulsar://192.168.56.2:6650")
+    .option("admin.url", "http://192.168.56.2:8080")
+    .option("topic", "apache/pulsar/my-topic")
+    .option("startingOffsets", "latest")
+    .load()
+
+  lines.printSchema()
+
+//  val words = lines.select(col("value").cast("string"))
+//    .as[String].flatMap(_.split("\\s+"))
+
+//  val wc = words.groupBy("value").count()
+
+//  val query = wc.selectExpr("to_json(struct(*)) AS value")
+  val out = lines.select(col("__eventTime"), col("value").cast("string"))
+  out.printSchema()
+
+  val query = out.selectExpr("to_json(struct(*)) AS value").writeStream
+    .format("pulsar")
+//    .outputMode("complete")
+    .option("checkpointLocation", "/tmp/checkpoint")
+    .option("service.url", "pulsar://192.168.56.2:6650")
+    .option("admin.url", "http://192.168.56.2:8080")
+    .option("topic", "apache/pulsar/my-result-topic")
+    .start()
+
+    query.awaitTermination()
+}
