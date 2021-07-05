3d27996868ffc8a2f0e8e9f1e5f20ecb4936c911
nathluu <luuhoangtanbis@gmail.com>
2021-07-05 15:48:25 +0700

    jwt auth

--- spark/pulsar-spark-wordcount/src/main/scala/com/example/spark/jobs/WordCount.scala
+++ spark/pulsar-spark-wordcount/src/main/scala/com/example/spark/jobs/WordCount.scala
@@ -12,16 +12,16 @@ object WordCount extends App {
   import spark.implicits._
   spark.sparkContext.setLogLevel("ERROR")
 
-  val certPath = getClass.getResource("/ca.cert.pem").getPath
+//  val certPath = getClass.getResource("/ca.cert.pem").getPath
   val lines = spark.readStream
     .format("pulsar")
-    .option("service.url", "pulsar+ssl://192.168.243.2:6651")
-    .option("admin.url", "https://192.168.243.2:8443")
+    .option("service.url", "pulsar://192.168.56.2:6650")
+    .option("admin.url", "http://192.168.56.2:8080")
     .option("pulsar.client.authPluginClassName", "org.apache.pulsar.client.impl.auth.AuthenticationToken")
-    .option("pulsar.client.authParams", "token:eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXBlci11c2VyIiwiZXhwIjoxNjU1MTk5NTgzfQ.ae6sHHtMwuLP0on-0Gvcmy7bnDCTSDhcnt_r41NCCjo")
-    .option("pulsar.client.tlsTrustCertsFilePath", certPath)
-    .option("pulsar.client.tlsAllowInsecureConnection", "false")
-    .option("pulsar.client.tlsHostnameVerificationEnable", "false")
+    .option("pulsar.client.authParams", "token:eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.etXkxEP3vJlKIuwO7YRdr7IVSvnCD4107jLacuYIdBo")
+//    .option("pulsar.client.tlsTrustCertsFilePath", certPath)
+//    .option("pulsar.client.tlsAllowInsecureConnection", "false")
+//    .option("pulsar.client.tlsHostnameVerificationEnable", "false")
     .option("topic", "apache/pulsar/my-topic")
     .option("startingOffsets", "latest")
     .load()
@@ -31,13 +31,13 @@ object WordCount extends App {
   val query = out.writeStream
     .format("pulsar")
     .option("checkpointLocation", "/tmp/checkpoint")
-    .option("service.url", "pulsar+ssl://192.168.243.2:6651")
-    .option("admin.url", "https://192.168.243.2:8443")
+    .option("service.url", "pulsar://192.168.56.2:6650")
+    .option("admin.url", "http://192.168.56.2:8080")
     .option("pulsar.client.authPluginClassName", "org.apache.pulsar.client.impl.auth.AuthenticationToken")
-    .option("pulsar.client.authParams", "token:eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXBlci11c2VyIiwiZXhwIjoxNjU1MTk5NTgzfQ.ae6sHHtMwuLP0on-0Gvcmy7bnDCTSDhcnt_r41NCCjo")
-    .option("pulsar.client.tlsTrustCertsFilePath", certPath)
-    .option("pulsar.client.tlsAllowInsecureConnection", "false")
-    .option("pulsar.client.tlsHostnameVerificationEnable", "false")
+    .option("pulsar.client.authParams", "token:eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.etXkxEP3vJlKIuwO7YRdr7IVSvnCD4107jLacuYIdBo")
+//    .option("pulsar.client.tlsTrustCertsFilePath", certPath)
+//    .option("pulsar.client.tlsAllowInsecureConnection", "false")
+//    .option("pulsar.client.tlsHostnameVerificationEnable", "false")
     .option("topic", "apache/pulsar/my-result-topic")
     .start()
 
